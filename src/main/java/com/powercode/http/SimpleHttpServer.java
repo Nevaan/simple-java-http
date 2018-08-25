56d860029cd222754c53e93ba80150ab72392bb3
Pawel <pawel.losek@outlook.com>
2018-08-25 13:59:07 +0200

    Repository init

---
+++ src/main/java/com/powercode/http/SimpleHttpServer.java
@@ -0,0 +1,7 @@
+package com.powercode.http;
+
+public class SimpleHttpServer {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/powercode/http/SimpleHttpServerTest.java
@@ -0,0 +1,14 @@
+package com.powercode.http;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SimpleHttpServerTest {
+
+    @Test
+    void justAnExample() {
+        assertTrue(true);
+    }
+
+}
